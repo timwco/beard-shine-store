1e1432c9b2bcb7733b7003b95025490c77e2b8bc
Tim Whitacre <tim@timw.co>
2016-12-03 23:43:12 -0500

    pages moved over

--- src/js/utils/config.js
+++ src/js/utils/config.js
@@ -2,22 +2,32 @@ function config ($urlRouterProvider, $stateProvider) {
 
  $stateProvider
 
+  // Home Page
   .state('layout', {
     abstract: true,
-    templateUrl: 'templates/layout.html'
+    templateUrl: 'templates/layouts/layout.html'
   })
   .state('layout.home', {
     url: '/',
-    templateUrl: 'templates/home.html'
+    templateUrl: 'templates/pages/home.html'
   })
 
+  // Interior Pages
   .state('inner', {
     abstract: true,
-    templateUrl: 'templates/layout-inner.html',
+    templateUrl: 'templates/layouts/layout-inner.html',
   })
   .state('inner.about', {
     url: '/about',
-    templateUrl: 'templates/about.html'
+    templateUrl: 'templates/pages/about.html'
+  })
+  .state('inner.contact', {
+    url: '/contact',
+    templateUrl: 'templates/pages/contact.html'
+  })
+  .state('inner.faq', {
+    url: '/faq',
+    templateUrl: 'templates/pages/faq.html'
   })
 
 
