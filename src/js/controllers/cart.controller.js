09129461fb40a1c7d2fe91b02af221992dc63b51
Tim Whitacre <tim@timw.co>
2016-12-10 12:17:03 -0500

    working on cart - subtotal working

--- src/js/controllers/cart.controller.js
+++ src/js/controllers/cart.controller.js
@@ -1,17 +1,27 @@
-function CartController (ShopifyService) {
+function CartController (ShopifyService, $state) {
 
   let vm = this;
+ 
+  vm.removeItem = removeItem;
 
   init();
 
   function init() {
-
     ShopifyService.getCart().then( cart => {
       vm.products = cart.lineItems;
+      vm.subtotal = cart.subtotal;
     });
   }
 
+  function removeItem(id) {
+    ShopifyService.getCart().then( cart => {
+      cart.removeLineItem(id).then( cart => {
+        $state.reload();
+      });
+    });
+  } 
+
 }
 
-CartController.$inject = ['ShopifyService'];
+CartController.$inject = ['ShopifyService', '$state'];
 export { CartController };
